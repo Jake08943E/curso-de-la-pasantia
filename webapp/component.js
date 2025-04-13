b2c138e294f5424fb2d256ef4b9ded1a68d938ef
prueba_git <andymcabreragrullon@gmail.com>
2025-04-13 04:53:08 -0400

    a ca agregue una descripcion a la app

--- webapp/component.js
+++ webapp/component.js
@@ -6,17 +6,10 @@ sap.ui.define([
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata : {
-            rootView: {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                "async": true,
-                "id": "app"
-            }
+            manifest: "json"
         },
         init : function () {
-            // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
-            // set data models
             var oData = {
                 recipient: {
                     name: "UI5"
@@ -25,13 +18,6 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.setModel(i18nModel, "i18n")
         }
     });
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -11,4 +11,4 @@ sap.ui.define([
             MessageToast.show(sMsg);
         }
     });
-});
+});  
