3aab8b2ab7c8e3c0520097d756aad3bff12a8778
prueba_git <andymcabreragrullon@gmail.com>
2025-04-13 18:20:49 -0400

    a ca agreguelos dialogues and fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/core/Fragment"
-], function (Controller, MessageToast, Fragement) {
+], function (Controller, MessageToast, Fragment) { 
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
         onShowHello: function () {
@@ -15,11 +15,10 @@ sap.ui.define([
         },
         onOpenDialog : function () {
             var oView = this.getView();
-
             // create the dialog lazily
             if (!this.byId("helloDialog")) {
                 // load asynchronous XML fragment
-                Fragement.load({
+                Fragment.load({  
                     id: oView.getId(),
                     name: "sap.ui.demo.walkthrough.view.HelloDialog"
                 }).then(function (oDialog) {
