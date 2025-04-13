56b47335a3ae4ae17e86c46b9c9c0eeeed941de1
prueba_git <andymcabreragrullon@gmail.com>
2025-04-13 03:23:22 -0400

    a ca agregue un modelo de JSON

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "use strict"
-    return Controller.extend("sap.ui.demo.walkthrough.App",{
-        onShowHello : function () {
-            // Show a native or vanailla JS alert
+    return Controller.extend("sap.ui.demo.walkthrough.App", {
+        onInit: function () {
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
+        onShowHello: function () {
             MessageToast.show("Hello there!")
         }
     });
