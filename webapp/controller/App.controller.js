f62589b722ffb2e61828fe995039e6415f91b29a
prueba_git <andymcabreragrullon@gmail.com>
2025-04-12 23:29:45 -0400

    a ca agregue un modulo segun el tutorial

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onShowHello : function () {
-            alert("Hello there!");
+            // Show a native or vanailla JS alert
+            MessageToast.show("Hello there!")
         }
     });
 });
