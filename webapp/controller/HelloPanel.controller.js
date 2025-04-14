afe4d6cfb5a26b9ef9a0f1f6997d89d6b3eae6a9
prueba_git <andymcabreragrullon@gmail.com>
2025-04-14 10:30:21 -0400

    a ca agregue los calls fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/core/Fragment"
-], function (Controller, MessageToast, Fragment) { 
+], function (Controller, MessageToast, Fragement) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
         onShowHello: function () {
@@ -13,14 +13,16 @@ sap.ui.define([
             // Show message
             MessageToast.show(sMsg);
         },
-        onOpenDialog : function () {
+        onOpenDialog: function () {
             var oView = this.getView();
+
             // create the dialog lazily
             if (!this.byId("helloDialog")) {
                 // load asynchronous XML fragment
-                Fragment.load({  
+                Fragement.load({
                     id: oView.getId(),
-                    name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                    controller: this
                 }).then(function (oDialog) {
                     // connect dialog to the root view of this component (models, lifecycle)
                     oView.addDependent(oDialog);
@@ -29,6 +31,9 @@ sap.ui.define([
             } else {
                 this.byId("helloDialog").open();
             }
+        },
+        onCloseDialog: function () {
+            this.byId("helloDialog").close();
         }
     })
 });
